aad3776b41b4d2c8ebe4a02ceefa3b082944958c
Jumman55 <jumman.1948848@studenti.uniroma1.it>
2022-11-30 00:11:22 +0100

    Initial Commit

---
+++ MonthlyBuget/MonthlyBugetApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonthlyBugetApp.swift
+//  MonthlyBuget
+//
+//  Created by Jumman Hossen on 30/11/22.
+//
+
+import SwiftUI
+
+@main
+struct MonthlyBugetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
